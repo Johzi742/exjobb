cd4ab429b347ea8edcce0201c7ed14a2531bb53f
Björn Wallner <bjornwallner@gmail.com>
2018-01-24 15:35:33 +0100

    added example to make density file

---
+++ InterCompLig.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # InterComp for Small-Molecule Ligands #
+# 
+# Adapt [InterComp](https://doi.org/10.1101/235812) for comparison of small-molcule ligands.
+# 
+# This includes:
+# 
+#     * Assessing statistical significance for small-molcule comparisons
+#     * Chemical similarity
+#     * Benchmark against similar software LIGSIFT
+# 
+# Similar software is described here:
+# http://cssb.biology.gatech.edu/LIGSIFT
+# 
+# 
--- wiki.py
+++ wiki.py
@@ -158,7 +158,7 @@ ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=10
 ts = ts.cumsum()
 
 df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
-                  columns=['A', 'B', 'C', 'D'])
+                  columns=['Asdfd', 'B', 'C', 'D'])
 df = df.cumsum()
 df.plot(); plt.legend(loc='best')
 
